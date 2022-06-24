05d82859640995e5cc1abb5429b5fdfbe50d5087
Leonardo Hornig <leohr.dev@gmail.com>
2022-06-24 13:20:18 -0300

    Create a component to the avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/leo22ca.png"/>
+                    <Avatar src="https://github.com/leo22ca.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Leonardo Hornig</strong>
                         <span>Software Developer</span>
